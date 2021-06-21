16a5d5181f84e32e5655ced8b8268f7469622d04
Firuz <vfiruz97@gmail.com>
2021-06-21 11:13:15 +0500

    initial && install vue and bootstrap

---
+++ resources/js/Pages/MainPage.vue
@@ -0,0 +1,19 @@
+<template>
+  <div>
+    <product-form></product-form>
+  </div>
+</template>
+
+<script>
+import ProductForm from "./../Components/ExampleComponent";
+
+export default {
+  data() {
+    return {};
+  },
+  components: {
+    ProductForm,
+  },
+  methods: {},
+};
+</script>
